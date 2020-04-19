ed11c57f93b05236bc3dc088e2088b7559d04547
theCoffee-app <35709628+theCoffee-app@users.noreply.github.com>
2020-04-19 12:13:29 -0600

    Added Blogs feature

---
+++ javascript.js
@@ -0,0 +1,98 @@
+var allButton = document.getElementById("allBtn");
+var trainButton = document.getElementById("trainBtn");
+var reviewButton = document.getElementById("reviewBtn");
+var leakButton = document.getElementById("leakBtn");
+
+var leakCards = document.getElementById("sneakLeaks");
+var trainCards = document.getElementById("training");
+var reviewCards = document.getElementById("review");
+
+
+allButton.addEventListener("click", showAll);
+trainButton.addEventListener("click", showTrain);
+reviewButton.addEventListener("click", showReview);
+leakButton.addEventListener("click", showLeak);
+
+allButton.addEventListener("click", setActive);
+//trainButton.addEventListener("click", setActive);
+//reviewButton.addEventListener("click", setActive);
+leakButton.addEventListener("click", setActive);
+
+
+function showAll() {
+    $(".card").css("display", "block");
+    
+//    $("#trainBtn").css({"color": "black",
+//                        "background-color": "white"
+//                      });
+//      $(".active").css({"color": "#fff",
+//                        "background-color": "#007bff"
+//                      });
+}
+
+function showTrain() {
+    
+    if (trainCards.style.display === "none" || trainCards.style.display === "null") {
+        $("div").filter("#training").css("display", "block");
+        $("div").filter("#review, #sneakLeaks").css("display", "none");
+    }
+    else {
+        $("div").filter("#review, #sneakLeaks").css("display", "none");
+    }
+    
+//    $("#trainBtn").css({"color": "#fff",
+//                        "background-color": "#007bff"
+//                      });
+//    $(".active").css({"color": "black",
+//                        "background-color": "white"
+//                      });
+}
+
+function showReview() {
+    
+    if (reviewCards.style.display === "none" || reviewCards.style.display === "null") {
+        $("div").filter("#review").css("display", "block");
+        $("div").filter("#training, #sneakLeaks").css("display", "none");
+    }
+    else {
+        $("div").filter("#training, #sneakLeaks").css("display", "none");
+    }
+    
+    
+    
+//    $("#reviewBtn").css({"color": "#fff",
+//                        "background-color": "#007bff"
+//                      });
+//    $(".active").css({"color": "black",
+//                        "background-color": "white"
+//                      });
+}
+
+function showLeak() {
+
+    if (leakCards.style.display === "none" || leakCards.style.display === "null") {
+        $("div").filter("#sneakLeaks").css("display", "block");
+        $("div").filter("#training, #review").css("display", "none");
+    }     
+    else {
+         $("div").filter("#training, #review").css("display", "none"); 
+         
+    } 
+}
+
+
+
+function setActive() {
+    //var navLink = document.getElementsByClassName('nav-link');
+    
+    if (this.className === "nav-link active") {
+    //if (this.className === "nav-link" && this.className === "active") {
+        this.className = "nav-link";
+        console.log("You aren't active");
+    } else {
+        this.className += " active";
+        console.log("You're Active!");
+    }
+}
+
+//TODO: add loop to check if button is greater than zero, than add active
